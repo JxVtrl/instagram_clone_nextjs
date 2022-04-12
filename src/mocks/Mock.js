222654cd5b2ac7141af859c8f40247bf3c15af39
Joao <joaoviniciusvitral@hotmail.com>
2022-04-12 17:04:34 -0300

    feat: mocks

---
+++ src/mocks/Mock.js
@@ -0,0 +1,81 @@
+export const Mock = {
+    "users": [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "username": "Bret",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "username": "Antonette",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "username": "Samantha",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "username": "Karianne",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "username": "Kamren",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 6,
+            "name": "Mrs. Dennis Schulist",
+            "username": "Leopoldo_Corkery",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 7,
+            "name": "Kurtis Weissnat",
+            "username": "Elwyn.Skiles",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 8,
+            "name": "Nicholas Runolfsdottir V",
+            "username": "Maxime_Nienow",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 9,
+            "name": "Glenna Reichert",
+            "username": "Delphine",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 10,
+            "name": "Clementina DuBuque",
+            "username": "Moriah.Stanton",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        },
+        {
+            "id": 11,
+            "name": "Nicholas DuBuque",
+            "username": "Nicholas.Stanton",
+            "email": "",
+            "picture": "https://picsum.photos/200/300"
+        }
+    ]
+}
