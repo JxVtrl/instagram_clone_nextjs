88200dd454220efb79a52f7c6340a0de41be80ee
Joao <joaoviniciusvitral@hotmail.com>
2022-04-12 17:05:06 -0300

    feat: pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyle from '../src/styles/global'
+import theme from '../src/styles/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
