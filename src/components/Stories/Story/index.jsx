f7247511a901cc2be7a9aa3f37e28d9aadbc238d
Joao <joaoviniciusvitral@hotmail.com>
2022-04-12 18:49:45 -0300

    feat: Story styles

--- src/components/Stories/Story/index.jsx
+++ src/components/Stories/Story/index.jsx
@@ -1,44 +1,12 @@
 import styled from 'styled-components';
 import Image from "next/image";
 import { StoryEllipseIcon } from '../../../assets'
-
-const StoryContainer = styled.div`
-    width: 68px;
-    height: 94px;
-    p {
-        margin: 0;
-        font-family: 'SF UI Display';
-        font-weight: 400;
-        font-size: 11.5px;
-        line-height: 22px;
-        text-align: center;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-`
-
-const PictureContainer = styled.div`
-    width: 68px;
-    height: 68px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-
-    svg {
-        position: absolute;
-        width: 68px;
-        height: 68px;
-    }
-`
-
+import { StoryContainer, PictureContainer } from './styles';
 
 const Story = item => {
     const name = item.user.name;
     const username = item.user.username;
     const src = item.user.picture;
-
-    console.log(src)
     
     return (
         <StoryContainer>
---
+++ src/components/Stories/Story/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const StoryContainer = styled.div`
+    width: 68px;
+    height: 94px;
+    p {
+        margin: 0;
+        font-family: 'SF UI Display';
+        font-weight: 400;
+        font-size: 11.5px;
+        line-height: 22px;
+        text-align: center;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+`
+
+export const PictureContainer = styled.div`
+    width: 68px;
+    height: 68px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    svg {
+        position: absolute;
+        width: 68px;
+        height: 68px;
+    }
+`
