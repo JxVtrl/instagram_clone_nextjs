63b4b5b00514ed3137136968aba6f670adfde90f
Joao <joaoviniciusvitral@hotmail.com>
2022-04-22 14:48:11 -0300

    feat: Data Mock key words

---
+++ src/mocks/Data.js
@@ -0,0 +1,24 @@
+export const Data = {
+    "themes": [
+        "rainbow",
+        "sea",
+        "forest",
+        "mountain",
+        "desert",
+        "space",
+        "food",
+        "pet",
+        "cat",
+        "dog",
+        "bird",
+        "nature",
+        "people",
+        "animal",
+        "car",
+        "transport",
+        "building",
+        "house",
+        "city",
+        "country",
+    ]
+}
