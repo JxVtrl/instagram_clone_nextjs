a9d3da267d79e86802b1a124ccc2b30894aa57e9
Joao <joaoviniciusvitral@hotmail.com>
2022-04-22 14:47:03 -0300

    feat: Mock update

--- src/mocks/Mock.js
+++ src/mocks/Mock.js
@@ -5,84 +5,113 @@ export const Mock = {
             "name": "João Vitral",
             "username": "JxVtrl",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 1,
             "name": "Leanne Graham",
             "username": "Bret",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 2,
             "name": "Ervin Howell",
-            "username": "Antonette",
+            "username": "Antote",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 3,
             "name": "Clementine Bauch",
             "username": "Samantha",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 4,
             "name": "Patricia Lebsack",
             "username": "Karianne",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 5,
             "name": "Chelsey Dietrich",
             "username": "Kamren",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 6,
             "name": "Mrs. Dennis Schulist",
             "username": "Leopoldo_Corkery",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 7,
             "name": "Kurtis Weissnat",
             "username": "Elwyn.Skiles",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 8,
             "name": "Nicholas Runolfsdottir V",
             "username": "Maxime_Nienow",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 9,
             "name": "Glenna Reichert",
             "username": "Delphine",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 10,
             "name": "Clementina DuBuque",
             "username": "Moriah.Stanton",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         },
         {
             "id": 11,
             "name": "Nicholas DuBuque",
             "username": "Nicholas.Stanton",
             "email": "",
-            "picture": "https://picsum.photos/200/300"
+            "picture": ""
         }
+    ],
+    "posts": [
+        {
+            "id": 0,
+            "src": "https://images.unsplash.com/photo-1650365449083-b3113ff48337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+            "likes": 1000,
+            "description": "aloasoaosa",
+            "comments": [],
+            "timestamp": "",
+            "sponsored": false,
+        },
+        {
+            "id": 3,
+            "src": "https://images.unsplash.com/photo-1641834248381-7177a625a2f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+            "likes": 10000,
+            "description": "Luiza Gatinha",
+            "comments": [],
+            "timestamp": "",
+            "sponsored": false,
+        },
+        {
+            "id": 5,
+            "src": "https://images.unsplash.com/photo-1615234435691-3b7bae98085e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+            "likes": 43450,
+            "description": "Hellooow",
+            "comments": [],
+            "timestamp": "",
+            "sponsored": true,
+        },
     ]
 }
