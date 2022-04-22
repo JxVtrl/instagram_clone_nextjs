66de1af6411ab32d8307683c984096ea350be3a2
Joao <joaoviniciusvitral@hotmail.com>
2022-04-22 14:46:45 -0300

    feat: Context update

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,23 +1,33 @@
 import React, { createContext, useContext, useEffect, useState, useRef } from 'react'
 import { getImageFromAPI } from '../services/unsplash/unsplash.js'
+import { Mock } from '../mocks/Mock.js'
 
 
 const AppContext = createContext()
 
 export function AppProvider({ children }) {
+    const [storiesActive, setStoriesActive] = useState(false)
+
+    useEffect(() => {
+        FindPhoto();
+    },[])
+
 
     function FindPhoto() {
         getImageFromAPI('profile')
-            .then(response => {
-                console.log(response)
-
-            })
+            .then(response => response.json())
+            .then(data => {
+                for (let i = 0; i < data.results.length; i++)
+                    Mock.users[i].picture = data.results[i].links.download
+                
+                setStoriesActive(true)
+            });
     }
 
 
     const value = {
         FindPhoto,
-     
+        storiesActive,
     }
 
     return (
