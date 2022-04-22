d30f32c0772c99cc557e56b6ab8947df56eca743
Joao <joaoviniciusvitral@hotmail.com>
2022-04-22 14:45:46 -0300

    feat: next config update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   env: {
     unsplashApi: process.env.NEXT_PUBLIC_UNSPLASH_API_KEY,
   },
+  images: {
+    domains: [
+      'unsplash.com',
+      'images.unsplash.com',
+      'www.freeiconspng.com',
+    ]
+  }
 }
 
 module.exports = nextConfig
